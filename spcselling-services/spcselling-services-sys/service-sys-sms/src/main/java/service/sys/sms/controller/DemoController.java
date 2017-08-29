c049ce4a0dcfbf35e49aa4813fb50d44c8296c74
xinxiamu <wnt*ZMT*701712%%>
2017-08-29 14:52:06 +0800

    调试

---
+++ spcselling-services/spcselling-services-sys/service-sys-sms/src/main/java/service/sys/sms/controller/DemoController.java
@@ -0,0 +1,28 @@
+package service.sys.sms.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private static final Logger LOGGER = LogManager.getLogger(DemoController.class);
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add(@RequestParam Integer a, @RequestParam Integer b) {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        Integer r = a + b;
+        LOGGER.info("/add, host:" + instance.getHost() + ":" + instance.getPort() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+        return "From service-sys-sms, Result is " + r;
+    }
+}
