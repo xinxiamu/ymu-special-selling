69a95683932d49461e11f4dd0c27586599e10333
xinxiamu <932852117@qq.com>
2017-08-31 23:49:28 +0800

    调试

---
+++ spcselling-services/spcselling-services-sys/service-sys-sms/src/main/java/service/sys/sms/common/Constants.java
@@ -0,0 +1,11 @@
+package service.sys.sms.common;
+
+public final class Constants {
+
+    public final static class ServiceAppName {
+
+        public final static String SERVICE_SYS_SMS = "service-sys-sms";
+        public final static String SERVICE_SYS_EMAIL = "service-sys-email";
+
+    }
+}
