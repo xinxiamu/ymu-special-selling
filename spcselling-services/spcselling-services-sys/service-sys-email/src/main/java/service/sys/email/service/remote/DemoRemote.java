e70d9d08b378aae79001f84756fd08f015d9586d
xinxiamu <wnt*ZMT*701712%%>
2017-08-31 20:29:48 +0800

    调试

---
+++ spcselling-services/spcselling-services-sys/service-sys-email/src/main/java/service/sys/email/service/remote/DemoRemote.java
@@ -0,0 +1,7 @@
+package service.sys.email.service.remote;
+
+/**
+ * Created by Administrator on 2017-08-31.
+ */
+public class DemoRemote {
+}
---
+++ spcselling-services/spcselling-services-sys/service-sys-email/src/main/java/service/sys/email/service/remote/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 调用远程服务
+ */
+package service.sys.email.service.remote;
