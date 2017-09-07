9cc3c0183cbe0ee645dbaf3a438c0e1117cc60e8
xinxiamu <932852117@qq.com>
2017-09-07 18:08:07 +0800

    调试

--- spcselling-server/src/main/java/com/ymu/spcselling/eureka/server/ReadingListServletInitializer.java
+++ spcselling-server/src/main/java/com/ymu/spcselling/eureka/server/ReadingListServletInitializer.java
@@ -1,15 +1,13 @@
 package com.ymu.spcselling.eureka.server;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 /**
  * @author mutian
  *
  */
-public class ReadingListServletInitializer extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(SpcsellingEurekaServerApp.class);
-	}
-}
+//public class ReadingListServletInitializer extends SpringBootServletInitializer {
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(SpcsellingEurekaServerApp.class);
+//	}
+//}
