6bf9f4a32e998f208e300525f4f7efbdc72ae3da
xinxiamu <932852117@qq.com>
2017-09-08 15:36:05 +0800

    调试

--- spcselling-services/spcselling-services-sys/service-sys-sms/src/main/java/service/sys/sms/service/remote/HelloServiceClient.java
+++ spcselling-services/spcselling-services-sys/service-sys-sms/src/main/java/service/sys/sms/service/remote/HelloServiceClient.java
@@ -2,14 +2,11 @@ package service.sys.sms.service.remote;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import service.sys.sms.api.HelloServiceApi;
-import service.sys.sms.api.SmsSendServiceApi;
 import service.sys.sms.common.Constants;
-import service.sys.sms.service.remote.hystrix.ServiceSysEMailHystrix;
+import service.sys.sms.service.remote.hystrix.HelloServiceClientlHystrix;
 
 @Component
-@FeignClient(value = Constants.ServiceAppName.SERVICE_SYS_EMAIL,fallback = ServiceSysEMailHystrix.class)
+@FeignClient(value = Constants.ServiceAppName.SERVICE_SYS_EMAIL,fallback = HelloServiceClientlHystrix.class)
 public interface HelloServiceClient extends HelloServiceApi {
 }
