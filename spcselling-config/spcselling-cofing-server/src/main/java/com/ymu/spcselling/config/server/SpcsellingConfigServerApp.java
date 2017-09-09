f2333b7eeafb910f969a84f4db560607dc537577
xinxiamu <932852117@qq.com>
2017-09-09 17:43:50 +0800

    调试

--- spcselling-config/spcselling-cofing-server/src/main/java/com/ymu/spcselling/config/server/SpcsellingConfigServerApp.java
+++ spcselling-config/spcselling-cofing-server/src/main/java/com/ymu/spcselling/config/server/SpcsellingConfigServerApp.java
@@ -1,13 +1,11 @@
 package com.ymu.spcselling.config.server;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableAutoConfiguration
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableConfigServer // 开启配置中心功能
