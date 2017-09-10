2ec5acac118471c4687a09b1be76e59cb048ba6a
xinxiamu <932852117@qq.com>
2017-09-10 22:52:20 +0800

    调试

---
+++ spcselling-clients/spcselling-facade/src/main/java/com/ymu/spcsellingfacade/SpcsellingFacadeApplication.java
@@ -0,0 +1,12 @@
+package com.ymu.spcsellingfacade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpcsellingFacadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpcsellingFacadeApplication.class, args);
+	}
+}
