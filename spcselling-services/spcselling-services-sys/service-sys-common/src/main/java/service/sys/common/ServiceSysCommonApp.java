8518dc6c8744fe77aed13bd1f4589536fa14b3a3
xinxiamu <932852117@qq.com>
2017-09-13 10:24:01 +0800

    调试

--- spcselling-services/spcselling-services-sys/service-sys-common/src/main/java/service/sys/common/ServiceSysCommonApp.java
+++ spcselling-services/spcselling-services-sys/service-sys-common/src/main/java/service/sys/common/ServiceSysCommonApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServiceSysCommonApplication {
+public class ServiceSysCommonApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceSysCommonApplication.class, args);
+		SpringApplication.run(ServiceSysCommonApp.class, args);
 	}
 }
--- spcselling-services/spcselling-services-sys/service-sys-common/src/test/java/service/sys/common/ServiceSysCommonAppTests.java
+++ spcselling-services/spcselling-services-sys/service-sys-common/src/test/java/service/sys/common/ServiceSysCommonAppTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ServiceSysCommonApplicationTests {
+public class ServiceSysCommonAppTests {
 
 	@Test
 	public void contextLoads() {
