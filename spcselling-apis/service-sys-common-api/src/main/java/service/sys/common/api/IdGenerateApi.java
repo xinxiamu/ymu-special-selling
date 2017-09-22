50481c487458856211282f9a22fefb959f7958af
xinxiamu <932852117@qq.com>
2017-09-22 14:33:58 +0800

    apidoc编写

--- spcselling-apis/service-sys-common-api/src/main/java/service/sys/common/api/IdGenerateApi.java
+++ spcselling-apis/service-sys-common-api/src/main/java/service/sys/common/api/IdGenerateApi.java
@@ -13,19 +13,89 @@ import javax.validation.Valid;
 @RequestMapping(SpcsConstants.API_VERSION + "/id")
 public interface IdGenerateApi {
 
+
+
     /**
-     * 生成分布式id。
-     * @param vIdGenReq
-     * @return
+     * 生成分布式id
+     * @param vIdGenReq 请求对象。body体
+     * @return 生成的系统全局唯一id
+     *
+     * @api {post} /v1/id/gen 生成分布式id
+     * @apiVersion 1.0.0
+     * @apiName genId
+     * @apiGroup ID
+     * @apiPermission admin
+     *
+     * @apiDescription 通过数据中心id，机器id生成long型唯一id
+     *
+     * @apiParam {long} dataCenterId 数据中心id,0-31。
+     * @apiParam {long} workerId 机器id，0-31。
+     *
+     * @apiParamExample {json} Request-Example:
+     *     Request Headers
+     *         Content-Type:application/json
+     *     body:
+     *     {
+     *       "dataCenterId": 0,
+     *       "workerId:" 0
+     *     }
+     *
+     * @apiExample 请求例子:
+     * curl -i http://localhost/user/4711
+     *
+     * @apiSuccess {long}   id      生成的id
+     *
+     * @apiError NoAccessRight 认证不通过
+     * @apiError UserNotFound   The <code>id</code> of the User was not found.
+     *
+     * @apiErrorExample 响应例子:
+     *     HTTP/1.1 401 Not Authenticated
+     *     {
+     *       "error": "NoAccessRight"
+     *     }
+     *
+     * @apiSampleRequest url
+     *
      */
     @PostMapping("/gen")
     long genId(@RequestBody @Valid VIdGenReq vIdGenReq);
 
     /**
-     * 解析分布式id。
+     *
+     * 解析分布式id
      * @param id
      * @return
+     *
+     * @api {post} /v1/id/expId  解析分布式id
+     * @apiVersion 1.0.0
+     * @apiName expId
+     * @apiGroup ID
+     * @apiPermission admin
+     *
+     * @apiDescription 把id解析成ID对象
+     *
+     * @apiParam {long} id 接口生成的id，必传。
+     *
+     * @apiExample 请求例子:
+     *  http://localhost/v1/id/expId?id=352608540609069079
+     *
+     * @apiSuccess {long}   timeStamp     时间戳。41位的时间序列
+     * @apiSuccess {long}   dataCenterId     数据中心id
+     * @apiSuccess {long}   workerId     节点机器id
+     * @apiSuccess {long}   sequence     序列号
+     *
+     * @apiError NoAccessRight 认证不通过
+     *  //@apiError UserNotFound   The <code>id</code> of the User was not found.
+     *
+     * @apiErrorExample 响应例子:
+     *     HTTP/1.1 401 Not Authenticated
+     *     {
+     *       "error": "NoAccessRight"
+     *     }
+     *
+     * @apiSampleRequest http://localhost/v1/id/expId
+     *
      */
     @GetMapping("/expId")
-    public ID expId(@RequestParam(value = "id") long id);
+    ID expId(@RequestParam(value = "id") long id);
 }
