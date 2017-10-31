4acf950d4d26a38b2375d8c35142fdcd24e98cdf
zmt <932852117@qq.com>
2017-10-31 17:41:22 +0800

    基础配置构建

--- spcselling-services/spsselling-services-basic/service-basic-user/src/test/java/service/basic/user/ServiceBasicUserAppTests.java
+++ spcselling-services/spsselling-services-basic/service-basic-user/src/test/java/service/basic/user/ServiceBasicUserAppTests.java
@@ -1,16 +1,9 @@
 package service.basic.user;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class ServiceBasicUserAppTests {
 
-	@Test
-	public void contextLoads() {
-	}
+
 
 }
