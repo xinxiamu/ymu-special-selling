fa2d839b9224028f847ee9aecbc2f0e98e809dbc
zmt <932852117@qq.com>
2017-11-01 18:07:12 +0800

    基础配置构建

--- spcselling-entity/entity-constants/src/main/java/com/ymu/spcselling/entity/constants/UserUsableType.java
+++ spcselling-entity/entity-constants/src/main/java/com/ymu/spcselling/entity/constants/UserUsableType.java
@@ -5,10 +5,10 @@ public enum UserUsableType {
     /**
      * 启用状态，此账号可以正常使用。
      */
-    USER_USABLE_ENABLE,
+    ENABLE,
 
     /**
      * 禁用状态，此账号不允许被使用。
      */
-    USER_USABLE_DISABLE
+    DISABLE
 }
