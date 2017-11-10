e0eca8e4429e31d037d1e58168df6c9f8f87fb4f
zmt <932852117@qq.com>
2017-11-10 15:10:47 +0800

    支付模块

---
+++ spcselling-services/spcselling-services-sys/service-sys-pay/src/main/java/com/ymu/spcselling/servicesyspay/ServiceSysPayApplication.java
@@ -0,0 +1,12 @@
+package com.ymu.spcselling.servicesyspay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSysPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceSysPayApplication.class, args);
+	}
+}
