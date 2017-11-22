4551ec9913f13cb7cfaefe74cd2ab2f8ff0d4ea5
zmt <932852117@qq.com>
2017-11-22 16:55:25 +0800

    基础配置

--- spcselling-services/spcselling-services-sys/service-sys-common/src/main/java/service/sys/common/config/MainConfig.java
+++ spcselling-services/spcselling-services-sys/service-sys-common/src/main/java/service/sys/common/config/MainConfig.java
@@ -32,10 +32,6 @@ public class MainConfig {
         return new SpringBeanFactory();
     }
 
-    /**
-     * 设置全局跨域。在WebConfig设置全局跨域失败(位置原因)，所以改成这样设置。
-     * @return
-     */
     @Bean
     public CorsFilter corsFilter(@Autowired CorsRegistrationConfig corsRegistrationConfig) {
         //1.添加CORS配置信息
@@ -47,13 +43,13 @@ public class MainConfig {
         //放行哪些原始域(请求方式)
         config.addAllowedMethod(corsRegistrationConfig.getAllowedMethods());
         //放行哪些原始域(头部信息)
-        config.addAllowedHeader(corsRegistrationConfig.getAllowedHeaders());
+        config.addAllowedHeader("*");
         //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
-//        config.addExposedHeader("*");
+//        config.addExposedHeader("header-1,header-2");
 
         //2.添加映射路径
         UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
-        configSource.registerCorsConfiguration(corsRegistrationConfig.getMapping(), config);
+        configSource.registerCorsConfiguration("/**", config);
 
         //3.返回新的CorsFilter.
         return new CorsFilter(configSource);
